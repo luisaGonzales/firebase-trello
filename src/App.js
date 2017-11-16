fc06efc6cbea06effb247736633e05ed515310d5
luisaGonzales <luisagc711@gmail.com>
2017-11-16 10:41:15 -0500

    Estructura de ruteo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 import {connect} from 'redux-zero/react'
 import './App.css';
-import Board from './components/Board/Board'
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Board from './components/Board/Board';
+// import SignIn from './components/signIn/signIn';
+// import SignUp from './components/signUp/signUp';
+
 
 const App = ({stages, tasks}) => {
   return(
-    <div>
-      <Board stages={stages} tasks={tasks}/>
-    </div>
+    <BrowserRouter>
+      <Switch>
+          {/* <Route exact path="/" render={() => <SignIn />} /> */}
+          {/* <Route exact path="/signIn" render={() => <SignIn />} />
+          <Route exact path="/signUp" render={() => <SignUp />} /> */}
+          <Route exact path="/boards" render={() => <Board stages={stages} tasks={tasks}/>} />
+          {/* <Route render={() => <Redirect to={"/"} />}/> */}
+      </Switch>
+      
+    </BrowserRouter>
 
   );
 }
+
 
 const mapToProps = ({stages, tasks})  => ({stages, tasks}) 
 export default connect(mapToProps)(App);
