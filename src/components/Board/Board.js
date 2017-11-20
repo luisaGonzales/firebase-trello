f409fca6478c66c76ec6b97b9a13d84d719cdc0e
luisaGonzales <luisagc711@gmail.com>
2017-11-20 11:43:19 -0500

    Estilos

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -5,43 +5,47 @@ import './Board.css';
 import {Button} from 'react-bootstrap';
 import {connect} from 'redux-zero/react';
 import {Redirect} from 'react-router-dom';
+import Header from '../Header/Header';
 
-const Board = ({stages, tasks, successLogin}) => {
-    const list = stages.map ( stage => {
-      return <Stage  key={stage} title={stage} 
-         tasks = {  tasks.filter ( e => e.stage === stage )}
-      />
-   });
-
-   return (
-      <div className = "Board-container">
-        {
-          !successLogin && <Redirect to = '/signIn' />
-        }
-          <div className = "Board-column">
-             {list}
-          </div>
-          <div className = "Board-column">
-            <form onSubmit = { (e) => {
-               e.preventDefault();
-               addStage (this.stageInputRef.value);
-            }}>
-               <input type="text" ref = {e => this.stageInputRef = e}/>
-               <button type="submit">
-                  save list
-               </button>
-               </form>
-            </div>
-            <Button type="button" className="btnSubmit" onClick={
-              () => {
-                  console.log("click");
-                  signOut()}
-          }>
-              Sign Out
-          </Button>
+const Board = ({stages, tasks, successLogin, user}) => {
+  const list = stages.map(stage => {
+    return <Stage
+      key={stage}
+      title={stage}
+      tasks=
+      { tasks.filter ( e => e.stage === stage )}/>
+  });
+  return (
+    <div className="Board-container">
+      {
+        !successLogin && <Redirect to='/signIn'/>
+      }
+      <Header name={"ol"}/>
+      <div className="Board-column">
+        {list}
       </div>
-   ); 
+      <div className="Board-column">
+        <form
+          onSubmit=
+          { (e) => { e.preventDefault(); addStage (this.stageInputRef.value); }}>
+          <input type="text" ref= {e => this.stageInputRef = e}/>
+          <button type="submit">
+            save list
+          </button>
+        </form>
+      </div>
+      <Button
+        type="button"
+        className="btnSubmit"
+        onClick={() => {
+        console.log("click");
+        signOut()
+      }}>
+        Sign Out
+      </Button>
+    </div>
+  );
 }
 
-const mapToProps = ({stages, tasks, successLogin}) => ({stages, tasks, successLogin});
-export default connect (mapToProps)(Board);
+const mapToProps = ({stages, tasks, successLogin, user}) => ({stages, tasks, successLogin, user});
+export default connect(mapToProps)(Board);
