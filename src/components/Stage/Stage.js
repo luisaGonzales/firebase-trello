92b746f4f48db1e65f6a96b0968351351c414d98
luisaGonzales <luisagc711@gmail.com>
2017-11-21 13:10:42 -0500

    Add

--- src/components/Stage/Stage.js
+++ src/components/Stage/Stage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Task from '../Task/Task'
+import './Stage.css';
 import {addTask} from '../../actions/actions';
 import {connect} from 'redux-zero/react';
 
@@ -11,18 +12,20 @@ const Stage = ({board, index, boardSelected, successLogin}) => {
             });
       }
       return (
-            <div>
+            <div className="margin">
                   <h3> {board.name}</h3>
-                  {list}
+                  <div className="listaStage">
+                        {list}
+                  </div>
                   <form onSubmit = { (e) => {
                         e.preventDefault();
                         console.log ('this.taskInputRef.value', this.taskInputReference.value)
                         addTask (boardSelected, index, this.taskInputReference.value );
                         this.taskInputReference.value = "";
                   }}>
-                  <input type="text" ref = {e => this.taskInputReference = e}/>
-                  <button type="submit">
-                        save card
+                  <input type="text" className="inputSaveCard" ref = {e => this.taskInputReference = e}/>
+                  <button type="submit" className="butonSaveCard">
+                        Save Card
                   </button>
                   </form>
             </div>
